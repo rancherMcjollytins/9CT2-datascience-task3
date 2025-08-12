07c9c5c41d734f3be9c72d488a77d272ea24855f
adrian <adrian.uy@education.nsw.gov.au>
2025-08-12 11:06:48 +1000

    bootsankatsa
    user interface, except no data import so no dataset view or graph view or filter or clean etc yet.

---
+++ datamain.py
@@ -0,0 +1,34 @@
+import time
+
+def main_menu():
+    while True:
+        print("=== Central Coast Crime Rates ===\n=== Data User Visualiser ===")
+        print()
+        print("1: View Dataset")
+        print("2: View Graphic Visualisation")
+        print("3: Filter Or Search")
+        print("4: Close Program")
+        print()
+        MenuInput = input("Choose an option by inputting a number between 1 - 4.  ")
+        if MenuInput == "1":
+            print("dataset go here")
+            print()
+            time.sleep(1)
+        elif MenuInput == "2":
+            print("imagine a cool graph and stuff here")
+            print()
+            time.sleep(1)
+        elif MenuInput == "3":
+            print("we filter here")
+            print()
+            time.sleep(1)
+        elif MenuInput == "4":
+            print("bye bye see you next time")
+            break
+        else:
+            print("I said a number from 1 to 4 do digit form this time")
+            print()
+            time.sleep(1)
+
+
+main_menu()
