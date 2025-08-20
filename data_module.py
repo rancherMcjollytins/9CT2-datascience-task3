f4c674ff1d1af5f872a4f099e2d1fa6dcd2dfa9e
rancherMcjollytins <adrian.uy@education.nsw.gov.au>
2025-08-20 10:21:30 +1000

    adrian, explain our eeffoc

--- data_module.py
+++ data_module.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 rawdata = pd.read_csv("dataset_majorcrime_centralcoast.csv")
 filterChoice = None
-rawdata.iloc[0:6 ,0]
-
+crimelist = ["Murder", "Non-Domestic Assault", "Breaking and Entering Dwelling", "Robbery", "Attempted Murder", "Sexual Assault"]
+visChoice = None
 
 
 def display_rawdata():
@@ -13,9 +13,33 @@ def display_rawdata():
     print(rawdata)
 
 def visualise_data():
-    print("Visualising!!!")
-    rawdata.plot(kind="line",x='Year',y=['Murder'], color=['red'],alpha=1,title='Central Coast Murder Rates Over Years')
-    plt.show()
+    for i in crimelist:
+        print(i)
+    print("Back")
+    visChoice = input("Please Choose a crime to visualise (1-7): ").strip()
+    if visChoice == "1":
+        rawdata.plot(kind="line",x='Year',y=['Murder'], color=['red'],alpha=1,title='Central Coast Murder Rates Over Years')
+        plt.show()
+    elif visChoice == "2":
+        rawdata.plot(kind="line",x='Year',y=['Non-Domestic Assault'], color=['orange'],alpha=1,title='Central Coast Non-Domestic Assault Rates Over Years')
+        plt.show()
+    elif visChoice == "3":
+        rawdata.plot(kind="line",x='Year',y=['Breaking and Entering Dwelling'], color=['purple'],alpha=1,title='Central Coast B.A.E Dwelling Rates Over Years')
+        plt.show()
+    elif visChoice == "4":
+        rawdata.plot(kind="line",x='Year',y=['Robbery'], color=['blue'],alpha=1,title='Central Coast Robbery Rates Over Years')
+        plt.show()
+    elif visChoice == "5":
+        rawdata.plot(kind="line",x='Year',y=['Attempted Murder'], color=['Green'],alpha=1,title='Central Coast Attempted Murder Rates Over Years')
+        plt.show()
+    elif visChoice == "6":
+        rawdata.plot(kind="line",x='Year',y=['Sexual Assault'], color=['Yellow'],alpha=1,title='Central Coast Sexual Assault Rates Over Years')
+        plt.show()
+    elif visChoice == "7":
+        print("Going Back!")
+        pass
+    else:
+        print("Please choose a number from 1 to 7!")
 
 def filter_data():
     print("=== Data Filtering ===")
@@ -25,7 +49,7 @@ def filter_data():
     filterChoice = input("=== Please Choose something to filter (1-3) ===\n Choose here: ").strip()
     if filterChoice == "1":
         print("choose crime")
-        print(rawdata.iloc[0,0:5])
+        print(crimelist)
     elif filterChoice == "2":
         pass
     elif filterChoice == "3":
